85ff432e38199ef278ff512e478184e5c275915a
ytrkptl <35569392+ytrkptl@users.noreply.github.com>
2020-06-08 07:19:53 -0400

    prettified

--- src/Actions.ts
+++ src/Actions.ts
@@ -1,31 +1,35 @@
-import { IAction, IEpisode, IState, Dispatch } from './interfaces'
+import { IAction, IEpisode, IState, Dispatch } from "./interfaces";
 
 export const fetchDataAction = async (dispatch: Dispatch) => {
   const URL =
-    'https://api.tvmaze.com/singlesearch/shows?q=family-guy&embed=episodes'
-  const data = await fetch(URL)
-  const dataJSON = await data.json()
+    "https://api.tvmaze.com/singlesearch/shows?q=family-guy&embed=episodes";
+  const data = await fetch(URL);
+  const dataJSON = await data.json();
   return dispatch({
-    type: 'FETCH_DATA',
-    payload: dataJSON._embedded.episodes
-  })
-}
+    type: "FETCH_DATA",
+    payload: dataJSON._embedded.episodes,
+  });
+};
 
-export const toggleFavAction = (state:IState, dispatch: any, episode: IEpisode | any): IAction => {
-  const episodeInFav = state.favorites.includes(episode)
+export const toggleFavAction = (
+  state: IState,
+  dispatch: any,
+  episode: IEpisode | any
+): IAction => {
+  const episodeInFav = state.favorites.includes(episode);
   let dispatchObj = {
-    type: 'ADD_FAV',
-    payload: episode
-  }
+    type: "ADD_FAV",
+    payload: episode,
+  };
   if (episodeInFav) {
     const favWithoutEpisode = state.favorites.filter(
       (fav: IEpisode) => fav.id !== episode.id
-    )
+    );
     dispatchObj = {
-      type: 'REMOVE_FAV',
-      payload: favWithoutEpisode
-    }
+      type: "REMOVE_FAV",
+      payload: favWithoutEpisode,
+    };
   }
 
-  return dispatch(dispatchObj)
-}
+  return dispatch(dispatchObj);
+};
